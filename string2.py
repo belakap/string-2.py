08d08833a51ac1c5d2fdf8c0e61b5d6703e53a6e
ngomba <belakap24@gmail.com>
2024-08-16 08:59:51 -0400

    string2.py

--- string2.py
+++ string2.py
@@ -9,8 +9,12 @@
 # Return the resulting string.
 
 def verbing(s):
-    # +++your code here+++
-    return
+    if len(s) < 3:
+        return s
+    if s.endswith('ing'):
+        return s + 'ly'
+    return s + 'ing'
+
 
 
 # E. not_bad
@@ -23,8 +27,13 @@ def verbing(s):
 # This dinner is good!
 
 def not_bad(s):
-    # +++your code here+++
-    return
+    not_index = s.find('not')
+    bad_index = s.find('bad')
+    
+    if not_index != -1 and bad_index != -1 and bad_index > not_index:
+        return s[:not_index] + 'good' + s[bad_index + len('bad'):]
+    
+    return s
 
 
 # F. front_back
@@ -36,8 +45,17 @@ def not_bad(s):
 #  a-front + b-front + a-back + b-back
 
 def front_back(a, b):
-    # +++your code here+++
-    return
+    def split_string(s):
+        mid = (len(s) + 1) // 2
+        front = s[:mid]
+        back = s[mid:]
+        return front, back
+    
+    a_front, a_back = split_string(a)
+    b_front, b_back = split_string(b)
+    
+    return a_front, b_front, a_back, b_back
+    
 
 
 # Simple provided test() function used in main() to print
